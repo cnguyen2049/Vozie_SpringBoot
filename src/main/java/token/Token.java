8ac51d920e258c48eeb5816d3a901864d0f17852
Chris Nguyen <python490@gmail.com>
2016-09-06 18:20:02 -0400

    First commit with initital prototype. Build in Spring Boot. Will process stripe token and charge card

---
+++ src/main/java/token/Token.java
@@ -0,0 +1,17 @@
+package token;
+
+/**
+ * Created by Nguyen on 5/31/16.
+ */
+public class Token {
+    private final String token;
+
+    public Token(String token){
+        this.token = token;
+    }
+
+    public String getToken(){
+        return token;
+    }
+
+}
---
+++ src/main/java/token/TokenController.java
@@ -0,0 +1,13 @@
+package token;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Created by Nguyen on 5/31/16.
+ */
+
+@RestController
+public class TokenController {
+	
+}
